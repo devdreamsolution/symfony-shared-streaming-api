2da982cbc5decbdc5d2ad8222ab4d1f0bf92704b
Devsolution0216 <q3construction1@gmail.com>
2020-05-19 02:13:14 +0800

    Create Message.php

---
+++ src/Entity/Message.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MessageRepository::class)
+ */
+class Message
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $sender_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $receiver_id;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $state;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $room_id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updated_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSenderId(): ?int
+    {
+        return $this->sender_id;
+    }
+
+    public function setSenderId(int $sender_id): self
+    {
+        $this->sender_id = $sender_id;
+
+        return $this;
+    }
+
+    public function getReceiverId(): ?int
+    {
+        return $this->receiver_id;
+    }
+
+    public function setReceiverId(int $receiver_id): self
+    {
+        $this->receiver_id = $receiver_id;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getState(): ?bool
+    {
+        return $this->state;
+    }
+
+    public function setState(bool $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getRoomId(): ?int
+    {
+        return $this->room_id;
+    }
+
+    public function setRoomId(int $room_id): self
+    {
+        $this->room_id = $room_id;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
