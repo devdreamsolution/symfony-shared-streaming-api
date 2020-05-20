cd31c8fdaa6c7809146f7911811276e277e954df
Devsolution <q3construction1@gmail.com>
2020-05-20 10:01:04 +0800

    Update UserController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,18 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/userlist", name="userlist")
      */
-    public function index()
+    public function userList()
     {
-        $user = new User();
+        $users = $this->getDoctrine()
+        ->getRepository(User::class)
+        ->find(1);
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
+            'users' => $users,
         ]);
     }
 }
