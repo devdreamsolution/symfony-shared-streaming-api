ed078ebd3526a71e4361d91738c45c4035105508
Devsolution <q3construction1@gmail.com>
2020-05-21 23:20:37 +0800

    Update DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,30 +10,6 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class DefaultController extends AbstractController
 {
-    public function register(Request $request, UserPasswordEncoderInterface $encoder)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $email = $request->request->get('_email');
-        $password = $request->request->get('_password');
-        $name = $request->request->get('_name');
-        $surename = $request->request->get('_surename');
-
-
-        $user = new User();
-        $user->setEmail($email);
-        $user->setRoles('ROLE_USER');
-        $user->setName($name);
-        $user->setSurename($surename);
-        $user->setCreatedAt();
-        $user->setPassword($encoder->encodePassword($user, $password));
-
-        $em->persist($user);
-        $em->flush();
-
-        return new Response(sprintf('User %s successfully created', $user->getRoles()));
-    }
-
     public function api()
     {
         return new Response(sprintf('Logged in as %s', $this->getUser()->getEmail()));
