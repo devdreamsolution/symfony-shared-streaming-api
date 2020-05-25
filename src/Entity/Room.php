b27a713e560455da9efae62d0f3d21aec1ef48b2
Devsolution <q3construction1@gmail.com>
2020-05-25 16:50:07 +0800

    Update Room.php

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\RoomRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +22,8 @@ class Room
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * Assert\Length(["max" => 100])
      */
     private $name;
 
@@ -37,11 +40,14 @@ class Room
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank()
+     * Assert\Length(["max" => 100])
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $qr_url;
 
