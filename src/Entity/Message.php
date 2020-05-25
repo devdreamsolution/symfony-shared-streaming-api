631080e41ab2c4fd2b79d1ecc51830f08d1b252e
Devsolution <q3construction1@gmail.com>
2020-05-25 16:50:05 +0800

    Update Message.php

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -6,6 +6,7 @@ use App\Repository\MessageRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=MessageRepository::class)
@@ -32,6 +33,7 @@ class Message
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Assert\NotNull
      */
     private $contents;
 
