583986b2f6e39378d229a028e94cc368d26527b9
Devsolution <q3construction1@gmail.com>
2020-05-27 22:17:53 +0800

    Create UserControllerTest.php

---
+++ tests/Controller/UserControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+
+	public function testUserRegister()
+	{
+		$client = static::createClient();
+
+		$client->request('POST', '/user/register', [
+			'username' => 'test@test.com',
+			'password' => 'testPassword',
+			'name' => 'testName',
+			'surename' => 'sureName',
+			'roles' => 0,
+			'city_residence' => 'testCity',
+			'group_age' => 10,
+			'gender' => 1,
+			'age' => 20,
+			'vat' => 10.2,
+			'address' => 'testAddress',
+			'lang' => 'en'
+		]);
+
+		$this->assertEquals(200, $client->getResponse()->getStatusCode());
+	}
+}
