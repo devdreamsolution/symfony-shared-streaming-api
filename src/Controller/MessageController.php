c2174f8257b205666288b2367d3dd062d392188f
Devsolution <q3construction1@gmail.com>
2020-05-29 22:15:10 +0800

    Update MessageController.php

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,13 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Message;
-use App\Entity\User;
 use App\Repository\RoomRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -33,32 +31,19 @@ class MessageController extends AbstractController
         
         $room_id = $request->request->get('room_id');
         $receiver_ids = $request->request->get('receiver_id');
-
-        if(!$room_id)
-        {
-            $responseArray['code'] = 400;
-            $responseArray['message'] = 'The room ID is null';
-            return new JsonResponse($responseArray);
-        }
-        if(!$receiver_ids)
-        {
-            $responseArray['code'] = 400;
-            $responseArray['message'] = 'The receiver ID is null';
-            return new JsonResponse($responseArray);
-        }
         $contents = $request->request->get('contents');
         
         $sender = $this->getUser();
         $room = $roomRepository->find($room_id);
         if(!$room)
         {
-            $responseArray['code'] = 404;
+            $responseArray['code'] = 400;
             $responseArray['message'] = 'The room is not existed.';
             return new JsonResponse($responseArray);
         }
         $status = false;
 
-        $message = new Message();
+        $message = new Message($sender, $room, $contents, $status);
         foreach($receiver_ids as $receiver_id)
         {
             $receiver = $userRepository->find($receiver_id);
@@ -70,10 +55,6 @@ class MessageController extends AbstractController
             }
             $message->addReceiver($receiver);
         }
-        $message->setSender($sender);
-        $message->setRoom($room);
-        $message->setContents($contents);
-        $message->setStatus($status);
 
         $errors = $validator->validate($message);
         if(count($errors) > 0)
