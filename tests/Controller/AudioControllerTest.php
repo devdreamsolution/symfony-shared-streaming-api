e07b63d1fb1643da986b785fcfa258710eb39881
Devsolution <q3construction1@gmail.com>
2020-05-29 22:15:27 +0800

    Update AudioControllerTest.php

--- tests/Controller/AudioControllerTest.php
+++ tests/Controller/AudioControllerTest.php
@@ -6,14 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AudioControllerTest extends WebTestCase
 {
+	private $username = 'guide@guide.com';					// Only the user who has ROLE_GUIDE
+	private $password = 'test';
 
+	/**
+	 * Audio create unit test
+	 */
 	public function testAudioCreate()
 	{
-		$client = static::createClient();
+		$client = static::createClient([], [
+			'PHP_AUTH_USER' => $this->username,
+			'PHP_AUTH_PW' => $this->password,
+		]);
 
 		$client->request('POST', '/audio/create', [
 			'room_id' => 5
 		]);
+
+		$this->assertEquals(200, $client->getResponse()->getStatusCode());
+	}
+
+	/**
+	 * Audio delete unit test
+	 */
+	public function testAudioDelete()
+	{
+		$client = static::createClient([], [
+			'PHP_AUTH_USER' => $this->username,
+			'PHP_AUTH_PW' => $this->password,
+		]);
+
+		$client->request('POST', '/audio/3/delete');
 
 		$this->assertEquals(200, $client->getResponse()->getStatusCode());
 	}
