6d61efc37d86185c757310713830d2ee2d0c5abe
Devsolution <q3construction1@gmail.com>
2020-06-23 16:40:11 +0800

    UserInfo

--- src/App/EventListener/AuthenticationSuccessListener.php
+++ src/App/EventListener/AuthenticationSuccessListener.php
@@ -11,6 +11,13 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class AuthenticationSuccessListener
 {
+    private $baseURL;
+
+    public function __construct()
+    {
+        $this->baseURL = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+    }
+
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
     {
         $data = $event->getData();
@@ -21,8 +28,12 @@ class AuthenticationSuccessListener
         }
 
         $data['userdata'] = array(
+            'id' => $user->getId(),
             'username' => $user->getUsername(),
             'roles' => $user->getRoles(),
+            'name' => $user->getName(),
+            'surename' => $user->getSurename(),
+            'picture' => $this->baseURL . '/' . $user->getPicture()
         );
 
         $event->setData($data);
